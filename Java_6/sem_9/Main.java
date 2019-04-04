b647076fd33b01a40e50c5faa477e34468cb93dc
Elena <helena557@mail.ru>
2019-04-05 00:16:53 +0300

    seminar 9

---
+++ Java_6/sem_9/Main.java
@@ -0,0 +1,58 @@
+public class Main {
+
+    static int counter = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> Main.inc2(true));
+        Thread thread2 = new Thread(() -> Main.inc2(false));
+
+        thread.start();
+        thread2.start();
+
+        thread.setName("thread one");
+        //thread.setPriority(1);
+        //thread.setDaemon(true);
+
+        System.out.println("End");
+
+        thread.join();
+        thread2.join();
+
+        Thread.currentThread().getName();
+
+        System.out.println("Absolute End");
+        System.out.println(counter);
+    }
+
+    private static void inc(boolean even){
+        for (int i = 0; i < 1000; i++) {
+            if (i % 2 == 0 == even) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static void inc2(boolean even){
+        for (int i = 0; i < 1000; i++) {
+            doIncrement();
+        }
+    }
+
+    private synchronized static void doIncrement(){
+        ++counter;
+    }
+
+    private static void inc3(boolean even){
+        for (int i = 0; i < 1000; i++) {
+            synchronized (Main.class) {
+                doIncrement();
+            }
+        }
+    }
+
+    private synchronized void run1(){
+        //non-static method
+    }
+
+
+}
---
+++ Java_6/sem_9/ProducerConsumer.java
@@ -0,0 +1,83 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ProducerConsumer {
+    private static final Deque<Object> objects = new ArrayDeque<>();
+
+    private static class Producer extends Thread{
+        @Override
+        public void run(){
+            while (true) {
+                synchronized (objects){
+                    if (objects.size() >= 1000){
+                        try {
+                            objects.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                synchronized (objects) {
+                    if (objects.size() < 1000) {
+                        continue;
+                    }
+
+                    long v = produce();
+                    synchronized (objects){
+                        objects.add(v);
+                        objects.notify();
+                    }
+                }
+            }
+        }
+
+        public long produce(){
+
+            return System.currentTimeMillis();
+        }
+    }
+
+    private static class Consumer extends Thread{
+        @Override
+        public void run(){
+            while (true){
+
+                synchronized (objects){
+                    if(objects.isEmpty()){
+                        try {
+                            objects.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                Object o;
+                synchronized (objects) {
+                    o = objects.pollFirst();
+                    objects.notify();
+                }
+
+                if (o != null){
+                    System.out.println(o);
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread producer = new Producer();
+        Thread consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.setName("Producer");
+        consumer.setName("Consumer");
+
+        //producer.join();
+        //consumer.join();
+    }
+}
